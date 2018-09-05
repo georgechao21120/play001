4253ecbadf6b23b3a33093f45b69ab6e5bbb2194
george <changheluori123@gmail.com>
2018-09-05 23:49:12 +0800

    no001

--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -1,12 +1,5 @@
 class MobileController < ApplicationController
   def index
-    @jobs = case params[:order]
-      @q = @jobs.ransack(params[:q])
-      @jobs = @q.result.order("id DESC").recent.paginate(:page => params[:page], :per_page => 6)
-  end
-  private
-
-  def job_params
-    params.require(:job).permit(:title, :description, :pos, :size, :echo, :comp, :form, :edge, :calc, :color, :spec, :elas, :spare, :symp, :sign, :bio, :path, :treat, :aut, :wage_upper_bound, :wage_lower_bound, :price, :image, :opare, :read, :apple, :ori, :seltrdes, :seltrpos, :seltrecho, :seltrcomp, :seltredge, :seltrcalc, :catamenta, :num, :paper, :person, :hct, :contact_email, :is_hidden, :remove_images, :images => [])
+  flash[:notice] = "早安！你好！"
   end
 end
